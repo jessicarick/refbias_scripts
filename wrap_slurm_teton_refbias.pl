c8ba63e516bbb639ea65e97e1c3115ed7564a39c
jessicarick <jrick@uwyo.edu>
2019-04-01 21:39:10 -0600

    small edits to wrapper script

--- wrap_slurm_teton_refbias.pl
+++ wrap_slurm_teton_refbias.pl
@@ -107,7 +107,7 @@ push @slurmdirectives, "#SBATCH --job-name=$jobname";
 push @slurmdirectives, "#SBATCH --time=$runtime"; 
 push @slurmdirectives, "#SBATCH --nodes=1";
 push @slurmdirectives, "#SBATCH --ntasks-per-node=16"; # 16 cores per node
-#push @slurmdirectives, "#SBATCH --mem=124G"; 
+push @slurmdirectives, "#SBATCH --mem=124G"; 
 push @slurmdirectives, "#SBATCH --workdir=$logdir";
 #          SLURM can send informative email messages to you about the
 #          status of your job.  
