b891c6291eb3bd9a3d39d8df5190676db708bcc8
Jessi Rick <rick08005@gmail.com>
2019-04-05 10:57:21 -0600

    changed to pull RAxML trees with bootstrap values

--- test_simphy_IntREFs_031119.sh
+++ test_simphy_IntREFs_031119.sh
@@ -228,8 +228,8 @@ mv *_config sim${sim}/config_files
 cp *.fa sim${sim}/ref.fasta_files
 				
 ###Create a batch file with all trees in order by file type, and create a name file
-cat s${sim}*q*miss*/*bestTree* >> ${output_dir}/${day}-${tree_height}-batch.trees
-ls s${sim}**q*miss*/*bestTree* >> ${output_dir}/${day}-${tree_height}-tree.names
+cat s${sim}*q*miss*/*bipartitions* >> ${output_dir}/${day}-${tree_height}-batch.trees
+ls s${sim}**q*miss*/*bipartitions* >> ${output_dir}/${day}-${tree_height}-tree.names
 
 #/project/phylogenref/scripts/astral_script_031119.sh $sim $tree_height $taxa_ref INT
 echo "done with RAxML analyses; now just waiting for ASTRAL to finish"
