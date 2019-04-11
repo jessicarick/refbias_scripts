0050297e3e8878122e404d9c5be23d43505af261
Jessi Rick <rick08005@gmail.com>
2019-04-11 13:56:48 -0600

    added rscript to calc_dxy script

--- calc_dxy.sh
+++ calc_dxy.sh
@@ -2,11 +2,17 @@
 
 PATH=$PATH:/home/bin/jrick/genomics_general
 
+## script for calculating pairwise Dxy
 input=$1 # vcf file to analyze
 output=`echo $input | sed 's/\.vcf//'`
+
+sim=$2
+tree_height=$3
+int=$4
+taxa_ref=$5
 
 python parseVCF.py -i $input --skipIndels | gzip > ${output}.geno.gz
 
 python distMat.py -g ${output}.geno.gz -f phased --windType cat -o ${output}.dist
 
-dxy_rscript 
+R CMD BATCH dxy_rscript.R
