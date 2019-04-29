4fb762cf787b6e2a4daa483495dae1b8d72ec53b
Jessi Rick <rick08005@gmail.com>
2019-04-29 15:32:38 -0600

    changed files to analyze

--- analysis/analyze_mutation_spectrum.R
+++ analysis/analyze_mutation_spectrum.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(ggpubr)
 
 ## Pulling info on simulated number of sites
-mut.files <- list.files(path="output/",pattern="041119-varSites*")
+mut.files <- list.files(path="output/",pattern="042419-varSites*")
 
 mut.all <- data.frame(gene=integer(),
                       variants=integer(),
@@ -17,14 +17,14 @@ for (i in 1:length(mut.files)){
   height <- as.integer(regmatches(mut.files[i], regexec('-([0-9]+)\\-sim', mut.files[i]))[[1]][2])
   int <- as.character(regmatches(mut.files[i], regexec('-([A-Z]+)', mut.files[i]))[[1]][2])
   mut.df <- data.frame(gene=seq(1,1000),
-                       variants=mut[-1],
+                       variants=varsites[-1],
                        sim_num=sim,
                        tree_height=height,
                        int_ext=int)
   mut.all <- rbind(mut.all,mut.df)
 }
 mut.all$tree_height <- as.factor(mut.all$tree_height)
-ggpubr::ggdensity(data=mut.all,x="variants",
+ggpubr::gghistogram(data=mut.all,x="variants",
                     color="tree_height",
                     fill="tree_height",
                     xlab="Variant Sites",
@@ -32,8 +32,11 @@ ggpubr::ggdensity(data=mut.all,x="variants",
                     add="mean")
 
 ## Pulling information from individual filtering combinations
-mut.filtered <- read.csv("output/041119-mutations.txt",header=TRUE,stringsAsFactors = FALSE)
+mut.filtered <- read.csv("output/042419-mutations.txt",header=TRUE,stringsAsFactors = FALSE)
 mut <- mut.filtered[mut.filtered$gene != "gene",]
+mut$num_nonInv<-as.numeric(mut$num_nonInv)
+mut$num_noRef<-as.numeric(mut$num_noRef)
+mut$num_SNPs<-as.numeric(mut$num_SNPs)
 mut$num_noRef[is.na(mut$num_noRef)] <- 0
 mut$num_nonInv[is.na(mut$num_nonInv)] <- 0
 
@@ -43,10 +46,11 @@ for (i in 1:nrow(mut)){
   mut$locus[i] <- as.integer(regmatches(mut$gene[i], regexec('gene([0-9]+)', mut$gene[i]))[[1]][2])
   mut$qual[i] <- as.integer(regmatches(mut$gene[i], regexec('q([0-9]+)\\_m', mut$gene[i]))[[1]][2])
   mut$miss[i] <- as.numeric(regmatches(mut$gene[i], regexec('miss(.*)\\_m', mut$gene[i]))[[1]][2])
-  mut$maf[i] <- as.numeric(regmatches(mut$gene[i], regexec('maf(0.*)', mut$gene[i]))[[1]][2])
+  mut$maf[i] <- as.numeric(regmatches(mut$gene[i], regexec('maf(0*)', mut$gene[i]))[[1]][2])
 }
 summary(mut)
 mut$tree_height <- as.factor(mut$tree_height)
+
 #mut$sim_num <- as.factor(mut$sim_num)
 
 ## combine the two tables together
@@ -55,7 +59,8 @@ mut.combined <- left_join(mut,mut.all[mut.all$int_ext == "EXT",],
                           by=c("locus" = "gene","sim_num","tree_height"))
 summary(mut.combined)
 mut.combined$diff <- mut.combined$variants - mut.combined$num_SNPs
-mut.combined$retained <- case_when(mut.combined$num_SNPs == 0 ~ "no",
+mut.combined$retained <- case_when(mut.combined$num_SNPs < 1 ~ "no",
                                    TRUE ~ "yes")
 
-hist(mut.combined$diff)
+ggdensity(data=mut.combined, x="num_SNPs", color="retained", fill="retained") +
+  geom_density(aes(mut.combined$num_SNPs))
