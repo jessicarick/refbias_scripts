260ca8e66c45e5fe5b490aebb8ce5b12e6e00553
jessicarick <jrick@uwyo.edu>
2019-06-02 07:45:12 -0600

    changed calculation of average to make sure it was doing the right thing

--- calc_dxy.sh
+++ calc_dxy.sh
@@ -16,6 +16,6 @@ parseVCF.py -i ${input} --skipIndels | gzip > ${output}.geno.gz
 
 distMat.py -g ${output}.geno.gz -f phased --windType cat -o ${output}.dist
 
-avg_dist=`grep "sim_${taxa_ref}" ${output}.dist | cut -f 2- -d' ' | awk '{s+=$1}END{print s/NR}' RS=" "`
+avg_dist=`grep "sim_${taxa_ref}" ${output}.dist | cut -f 2- -d' ' | sed 's/ /\n/g' | awk '{s+=$1} END {print s/NR}'`
 
 echo "$sim $tree_height $int $taxa_ref $avg_dist" >> ${output_dir}/${day}-refdist.txt
