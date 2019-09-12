7374fa1457f482c4bdd04cbf2636daf32c2720e3
Jessi Rick <rick08005@gmail.com>
2019-09-12 11:12:17 -0600

    new scripts

--- analysis/univariate_plots_refbias.R
+++ analysis/univariate_plots_refbias.R
@@ -2,6 +2,7 @@
 ## univariate plots
 ######################
 library(tidyverse)
+library(ggridges)
 
 output <- "053019-output"
 
@@ -9,14 +10,14 @@ results.raxml <- read.csv(paste("output/",output,"-raxml.csv",sep=""),header=TRU
 
 responses <- colnames(results.raxml[,13:28])
 
-pdf(paste("output/",output,"-univariate-plots.pdf",sep=""),width=11,height=8)
+pdf(paste("output/",output,"-univariate-plots_refdist_082819.pdf",sep=""),width=11,height=8)
 
 for (i in 1:length(responses)){
-  plot1 <- ggplot(data = results.raxml, aes(y=as.factor(maf),x=results.raxml[,responses[7]],color=factor(height),fill=factor(height)))
+  plot1 <- ggplot(data = results.raxml, aes(x=as.factor(results.raxml$refdist),y=results.raxml[,responses[i]]))
   
   plot2 <- plot1 +
-    #geom_boxplot(alpha=0.7, notch=FALSE, varwidth=FALSE,weight=2)+
-    geom_density_ridges(scale = 0.95, rel_min_height = 0.1, alpha = 0.5)+
+    geom_boxplot(alpha=0.7, notch=FALSE, varwidth=FALSE, weight=2)+
+    #geom_density_ridges(scale = 0.95, rel_min_height = 0.1, alpha = 0.5)+
     #scale_fill_manual(values=c("#009980", "#006699","turquoise","magenta","orange","green"),name="",aesthetics = "fill")+
     theme_classic()+
     theme(axis.title.y = element_text(angle=90, size=rel(2), face="plain"),
@@ -25,15 +26,15 @@ for (i in 1:length(responses)){
           axis.text.x = element_text(size=rel(2)),
           legend.text = element_text(size=rel(2)),
           legend.title = element_text(size=rel(2)),
-          plot.margin = unit(c(6,5.5,20,10),"points"),
+          #plot.margin = unit(c(6,5.5,20,10),"points"),
           line = element_line(size=1),
           panel.border = element_rect(color = "black", fill=NA, size=1),
           strip.text.x = element_text(size = 16))+
-    scale_x_continuous(name="RF Distance to True Tree")+
-    scale_y_discrete(name="MAF")+
+    scale_y_continuous(name=responses[i])+
+    scale_x_discrete(name="refdist")+
     #xlim(-50,10)+
-    facet_wrap(vars(int),nrow=2,strip.position = "top")+
-    geom_hline(yintercept=0,cex=2,lty=2,col="gray")+
+    #facet_wrap(vars(int),nrow=2,strip.position = "top")+
+    geom_hline(yintercept=0,cex=2,lty=2,col="gray")#+
     theme_ridges()
   
   # ylim1 = boxplot.stats(results.raxml[,responses[i]])$stats[c(1, 5)]
