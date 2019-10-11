a20d2ba86406ff3c098ef3ee5e00351cfe0368e9
jessicarick <jrick@uwyo.edu>
2019-10-11 13:44:43 -0600

    changes to analysis scripts

--- var_sites.R
+++ var_sites.R
@@ -4,7 +4,7 @@ options(echo=FALSE)
 args = commandArgs(trailingOnly=TRUE)
 nloci <- args[1]
 
-var_sites <- round(rnorm(nloci,mean=30,sd=10))
+var_sites <- round(rnorm(nloci,mean=50,sd=10))
 
 var_sites[var_sites < 1] <- 1
 
