cc43ab8dc442fb1cdf96b276411a4625ce1bee1e
Jessi Rick <rickx032@umn.edu>
2021-09-03 13:15:14 +0200

    streamlining scripts

--- filter_gene.sh
+++ filter_gene.sh
@@ -18,13 +18,8 @@ sites=`head -n 1 gene${gene}_miss${miss}_mac${mac}.REF.phy | awk '{print $2}'`
 
 if [ "$sites" -eq "0" ]; then 
 	echo "no sites left"
-	cat gene${gene}_miss${miss}_mac${mac}.REF.phy > gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy
+	#cat gene${gene}_miss${miss}_mac${mac}.REF.phy > gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy
 else 
-	#printf "library(ape)\nlibrary(phrynomics)\nReadSNP('gene${gene}_miss${miss}_mac${mac}.REF.phy',fileFormat='phy',extralinestoskip=1)->fullSNPs\nRemoveInvariantSites(fullSNPs, chatty=TRUE)->fullSNPs_only\nsnps <- RemoveNonBinary(fullSNPs_only, chatty=TRUE)\nWriteSNP(snps, file='gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy',format='phylip')" > Rscript_${gene}_miss${miss}_mac${mac}.R
-	
-	#R --vanilla --no-save < Rscript_${gene}_miss${miss}_mac${mac}.R
-	#rm -f Rscript_${gene}_miss${miss}_mac${mac}.R
-
 	python ${REF_PATH}/ascbias.py -p gene${gene}_miss${miss}_mac${mac}.REF.phy -o gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy
 	rm -f gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy.felsenstein
 	rm -f gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy.stamatakis
