c05abd4aff1889351901631b47b058c3b63e5c40
jessicarick <jrick@uwyo.edu>
2021-09-03 05:16:32 -0600

    updated results 082921

--- make_supermat.R
+++ make_supermat.R
@@ -4,11 +4,11 @@ options(echo=FALSE)
 args = commandArgs(trailingOnly=TRUE)
 prefix <- args[1]
 miss <- args[2]
-maf <- args[3]
+mac <- args[3]
 
 library(phylotools)
 
-pattern <- paste0("^gene[0-9]+_miss",miss,"_maf",maf,".[A-Z]+.noInv.phy")
+pattern <- paste0("^gene[0-9]+_miss",miss,"_mac",mac,".[A-Z]+.noInv.phy")
 infiles <- list.files(path=".",pattern=pattern)
 supermat(infiles, 
          outfile=paste(prefix,".all.noInv.phy",sep=""),
