f22de697291d60e9cf1e60de775729414f55fbdb
Jessi Rick <rickx032@umn.edu>
2021-09-03 09:31:26 +0200

    simplifying changes

--- filter_gene.sh
+++ filter_gene.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+source refbias_config.txt
+
 gene=$1
 
 echo "filtering for gene $gene, miss $miss, mac $mac"
@@ -11,20 +14,20 @@ python2 ${REF_PATH}/vcf2phylip.py \
 	-i gene${gene}_miss${miss}_mac${mac}.REF.recode.vcf \
 	-o gene${gene}_miss${miss}_mac${mac}.REF.phy  
 
-sites=`head -n 1 gene${gene}_miss${miss}_mac${mac}.REF.phy | awk '{print $2}'`
+#sites=`head -n 1 gene${gene}_miss${miss}_mac${mac}.REF.phy | awk '{print $2}'` # doesn't work for some reason
 
 if [ "$sites" -eq "0" ]; then 
 	echo "no sites left"
 	cat gene${gene}_miss${miss}_mac${mac}.REF.phy > gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy
 else 
-	printf "library(ape)\nlibrary(phrynomics)\nReadSNP('gene${gene}_miss${miss}_mac${mac}.REF.phy',fileFormat='phy',extralinestoskip=1)->fullSNPs\nRemoveInvariantSites(fullSNPs, chatty=TRUE)->fullSNPs_only\nsnps <- RemoveNonBinary(fullSNPs_only, chatty=TRUE)\nWriteSNP(snps, file='gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy',format='phylip')" > Rscript_${gene}_miss${miss}_mac${mac}.R
+	#printf "library(ape)\nlibrary(phrynomics)\nReadSNP('gene${gene}_miss${miss}_mac${mac}.REF.phy',fileFormat='phy',extralinestoskip=1)->fullSNPs\nRemoveInvariantSites(fullSNPs, chatty=TRUE)->fullSNPs_only\nsnps <- RemoveNonBinary(fullSNPs_only, chatty=TRUE)\nWriteSNP(snps, file='gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy',format='phylip')" > Rscript_${gene}_miss${miss}_mac${mac}.R
 	
-	R --vanilla --no-save < Rscript_${gene}_miss${miss}_mac${mac}.R
+	#R --vanilla --no-save < Rscript_${gene}_miss${miss}_mac${mac}.R
 	#rm -f Rscript_${gene}_miss${miss}_mac${mac}.R
 
-	#python /home/jrick/bin/raxml_ascbias/ascbias.py -p gene${gene}_miss${miss}_mac${mac}.REF.phy -o gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy
-	#rm -f gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy.felsenstein
-	#rm -f gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy.phy.stamatakis
+	python ${REF_PATH}/ascbias.py -p gene${gene}_miss${miss}_mac${mac}.REF.phy -o gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy
+	rm -f gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy.felsenstein
+	rm -f gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy.stamatakis
 fi 
 
 nsnps=`cat gene${gene}_miss${miss}_mac${mac}.REF.noInv.phy | head -n 1 | awk '{print $2}'`
