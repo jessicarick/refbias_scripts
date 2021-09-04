f6646c7458c80baf7c0cda9c867c8fe66747d7da
Jessi Rick <rickx032@umn.edu>
2021-09-04 10:59:36 +0200

    streamlining and tidying

---
+++ sim_scripts/run_bwa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ind=$1
+
+echo 'Mapping reads for ${ind}' 
+
+bwa mem ${reference_prefix}_sim${sim}.fa fastq_reads/sim${sim}/fastq/${ind}/${ind}_${sim}_read1.fq.gz fastq_reads/sim${sim}/fastq/${ind}/${ind}_${sim}_read2.fq.gz > aln_${ind}.sam
+
+echo 'Converting sam to bam for ${ind}' 
+samtools view -b -S -o aln_${ind}.bam aln_${ind}.sam
+
+echo 'Sorting and indexing bam files for ${ind}'
+samtools sort aln_${ind}.bam -o aln_${ind}.sorted.bam
+samtools index -c aln_${ind}.sorted.bam
