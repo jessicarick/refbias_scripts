1ac4a9b69831970b9467dec0695fc930f4e53bff
jessicarick <jrick@uwyo.edu>
2021-09-04 02:48:26 -0600

    tidying

---
+++ sim_scripts/make_supermat.R
@@ -0,0 +1,16 @@
+#!/bin/env Rscript
+
+options(echo=FALSE)
+args = commandArgs(trailingOnly=TRUE)
+prefix <- args[1]
+miss <- args[2]
+mac <- args[3]
+
+library(phylotools)
+
+pattern <- paste0("^gene[0-9]+_miss",miss,"_mac",mac,".[A-Z]+.noInv.phy")
+infiles <- list.files(path=".",pattern=pattern)
+supermat(infiles, 
+         outfile=paste(prefix,".all.noInv.phy",sep=""),
+         partition.file=paste(prefix,".all.partitions.txt",sep=""))
+
