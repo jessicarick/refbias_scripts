804c5736041974c5126a80cc3dc471befa71cfca
jessicarick <jrick@uwyo.edu>
2021-09-29 03:39:56 -0600

    new results 092321 + subsamp

--- analysis_script.sh
+++ analysis_script.sh
@@ -22,9 +22,9 @@ if [ ! -f output/new/${day}-all-raxml.trees ]; then
 		for name in `cat $names`;
 			do echo "${height}-${name}" >> output/new/${day}-all-raxml.names;
 		done;
-		if [ -f output/new/${day}-${height}-subsamp-batch.trees ]; then
+		if [ -f output/new/${day}-${height}-subsamp-tree.trees ]; then
 			echo "concatenating subsampled trees..."
-			sub_trees=output/new/${day}-${height}-subsamp-batch.trees
+			sub_trees=output/new/${day}-${height}-subsamp-tree.trees
 			sub_names=output/new/${day}-${height}-subsamp-tree.names
 			cat $sub_trees >> output/new/${day}-all-subsamp-raxml.trees
 			for name in `cat $sub_names`
