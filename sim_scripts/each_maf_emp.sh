a08aa5bc65030958861bdb4db7159b4ebd81aa2e
jessicarick <jrick@uwyo.edu>
2021-10-05 01:44:39 -0600

    moving and modifying scripts

---
+++ sim_scripts/each_maf_emp.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+source /project/phylogenref/scripts/sim_scripts/refbias_config_emp.txt
+
+mac=$1
+miss=$2
+
+echo "working with mac $mac, miss $miss"
+
+#if false; then 
+vcftools --vcf OUTFILE.q${QUAL}_s${sim}.${int}.vcf \
+         --out OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int} \
+         --remove-filtered-all \
+         --mac $(printf "$mac") \
+         --max-missing $(printf "$miss") \
+         --recode \
+         --recode-INFO-all \
+         --minDP 5 # keep the same?
+
+
+#######################################
+#### Running RaxML w/ Ref #############
+#######################################
+python2 ${REF_PATH}/sim_scripts/vcf2phylip.py \
+	-i OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.recode.vcf \
+	-o OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.phy  
+#fi
+
+if [[ -s OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.phy ]]; then
+	rm -f OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.recode.vcf
+else
+	echo "phylip file doesn't exist or is empty; exiting now"
+	exit 2
+fi
+
+sites=`head -n 1 OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.phy | awk '{print $2}'`
+
+#printf "library(ape)\nlibrary(phrynomics)\nReadSNP('OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}.phy',fileFormat='phy',extralinestoskip=1)->fullSNPs\nRemoveInvariantSites(fullSNPs, chatty=TRUE)->fullSNPs_only\nsnps <- RemoveNonBinary(fullSNPs_only, chatty=TRUE)\nWriteSNP(snps, file='OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}.noInv.phy',format='phylip')" > Rscript_miss${miss}_mac${mac}.R
+	
+#R --vanilla --no-save < Rscript_miss${miss}_mac${mac}.R
+#rm -f Rscript_miss${miss}_mac${mac}.R 
+
+python ${REF_PATH}/sim_scripts/ascbias.py \
+	-p OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.phy \
+	-o OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.noInv.phy
+
+rm -f OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.phy.felsenstein
+rm -f OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.phy.stamatakis
+
+nsnps=$(head -n 1  OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.noInv.phy | awk '{print $2}')
+echo "s${sim}_q${QUAL}_miss${miss}_mac${mac}.REF.${tree_height}.${int}.noInv,${nsnps}" >> ${output_dir}/${day}-SNPs-emp
+
+#sites_ref=`cat OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}.noInv.phy | head -n 1 | awk '{print $2}'`
+
+echo "running raxml on concatenated SNPs"
+raxmlHPC-PTHREADS-AVX -T 8 \
+	-s OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}_${int}.noInv.phy \
+	-n OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}.REF.${int}.emp.filtered.out \
+	-m ASC_GTRCAT -V \
+	--asc-corr=lewis \
+	-f a \
+	-x 223 \
+	-N 100 \
+	-p 466
+
+####Write number of sites to output file
+#echo "OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac},${sites_ref}" >> ${output_dir}/${day}-filteredSites-emp-${tree_height}-${int}
+
+####Add tree and name to output files
+cat RAxML*bipartitions.*s${sim}_q${QUAL}_miss${miss}_mac${mac}.REF.${int}.emp.filtered.out >> ${output_dir}/${day}-${tree_height}-emp-batch.trees
+ls RAxML*bipartitions.*s${sim}_q${QUAL}_miss${miss}_mac${mac}.REF.${int}.emp.filtered.out >> ${output_dir}/${day}-${tree_height}-emp-tree.names
+
+####Move RAxML results to a single directory
+if [ ! -d s${sim}_q${QUAL}.${int}-lates-emp.phylip_tree_files ]; then
+	mkdir s${sim}_q${QUAL}.${int}-lates-emp.phylip_tree_files
+fi
+
+mv *OUTFILE_s${sim}_q${QUAL}_miss${miss}_mac${mac}*.phy s${sim}_q${QUAL}.${int}-lates-emp.phylip_tree_files
+mv RAxML*s${sim}_q${QUAL}_miss${miss}_mac${mac}.REF.${int}.emp.filtered.out s${sim}_q${QUAL}.${int}-lates-emp.phylip_tree_files/
+
---
+++ sim_scripts/test_simphy_ExtREFs_082021_emp.sh
@@ -0,0 +1,190 @@
+#!/bin/sh
+
+#SBATCH --account=phylogenref
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks-per-node=16
+#SBATCH --nodes=1
+#SBATCH --array=1-10
+#SBATCH --mem=124G
+#SBATCH --no-requeue
+
+module load gcc
+module load miniconda3
+module load perl
+module load vcftools
+module load bwa
+module load samtools/1.6
+module load htslib
+module load bcftools
+module load raxml
+module load r
+module load parallel
+
+source activate new_env
+PATH=$PATH:/project/phylogenref/programs/art_bin_GreatSmokyMountains:/project/phylogenref/programs/TreeToReads:/project/phylogenref/programs/ASTRAL:/project/phylogenref/programs/SimPhy_1.0.2/bin:/project/phylogenref/programs/Seq-Gen-1.3.4/source
+
+#####################################
+####################################
+#####################################
+
+source /project/phylogenref/scripts/sim_scripts/refbias_config_emp.txt
+tree_height="cichlids"
+int=EXT
+day=100421
+sim=${SLURM_ARRAY_TASK_ID}
+
+cd /gscratch/jrick/phylogenref/emp_tmp/${tree_height}/
+
+if [ "$tree_height" == "lates" ]; then
+	if [ "$int" == "EXT" ]; then
+		taxa_ref="Lcal"
+		ref_genome="${REF_PATH}/references/lcalcarifer_genome_v3_chromosomal.fa"
+	else
+		taxa_ref="Lmar"
+		ref_genome="${REF_PATH}/references/lmariae_genome_Feb2018.fa"
+	fi
+elif [ "$tree_height" == "cichlids" ]; then
+	if [ "$int" == "EXT" ]; then
+		taxa_ref="Onil"
+		ref_genome="${REF_PATH}/references/oreochromis.fasta"
+	else
+		taxa_ref="Pnye"
+		ref_genome="${REF_PATH}/references/puncross.gapsEstimated.fasta"
+	fi
+else
+	echo "reference genome unknown; exiting now"
+	exit 1
+fi
+    
+echo "starting empirical $tree_height analysis for $int, simulation $sim"
+#declare fastq_list=`ls /project/phylogenref/data/${tree_height}/${int}_ref/*/*.fastq.gz | xargs -n 1 basename | sed 's/\.fastq\.gz//'`
+
+##############################################
+### Align to reference  ######################
+### Make sure everything in indexed! #########
+##############################################
+
+#export ref_genome
+
+#parallel --env ref_genome "echo 'Fastq: {} \n' && echo 'Mapping reads for {} \n' && bwa mem -t 16 ${ref_genome} /project/phylogenref/data/lates/{}.fastq.gz > aln_{}.sam && echo 'Converting sam to bam for {}' && samtools view -b -S -o aln_{}.bam aln_{}.sam && echo 'Sorting and indexing bam files for {} \n' && samtools sort aln_{}.bam -o aln_{}.sorted.bam && samtools index -c aln_{}.sorted.bam" ::: $fastq_list
+
+#rm -f *.sam
+#rm -f *[0-9].bam
+
+#fi #DEBUGGING
+#sims=1
+#for sim in `seq $sims`; do
+	
+	if [[ ! -s rand_ind_sim${sim}.txt ]]; then
+		if [ "$tree_height" == "lates" ]; then
+			ls /project/phylogenref/data/${tree_height}/${int}_ref/*.sorted.bam | grep -v 'SRR' | shuf -n 50 > rand_ind_sim${sim}.txt
+			echo "/project/phylogenref/data/${tree_height}/${int}_ref/aln_SRR3140997.Lcal.sorted.bam" >> rand_ind_sim${sim}.txt
+		elif [ "$tree_height" == "cichlids" ]; then
+			cat /project/phylogenref/data/${tree_height}/tropheine_bamfiles_highpct_100kReads | grep $int | grep -v 'SRR' | shuf -n 49 > rand_ind_sim${sim}.txt
+			ls /project/phylogenref/data/${tree_height}/${int}_ref/bwa_assem*/aln_SRR*.sorted.bam >> rand_ind_sim${sim}.txt
+		fi
+	elif [ "$tree_height" == "lates" ]; then
+                sed -i "s/INT/${int}/g" rand_ind_sim${sim}.txt
+                sed -i "s/EXT/${int}/g" rand_ind_sim${sim}.txt
+        elif [ "$tree_height" == "cichlids" & "$int" == "EXT" ]; then
+                sed -i 's#INT_ref/bwa_assem_pundamilia#EXT_ref/bwa_assem_oreochromis#g' rand_ind_sim${sim}.txt
+        elif [ "$tree_height" == "cichlids" & "$int" == "INT" ]; then
+		sed -i 's#EXT_ref/bwa_assem_oreochromis#INT_ref/bwa_assem_pundamilia#g' rand_ind_sim${sim}.txt
+	fi
+
+	QUAL=40	
+	if [[ ! -s OUTFILE_q${QUAL}_s${sim}.${int}_RN.bcf ]] && [[ ! -s OUTFILE.q${QUAL}_s${sim}.${int}.vcf ]]; then
+		samtools mpileup -g -t DP,AD \
+			--skip-indels \
+    	 	-P ILLUMINA \
+        	-q $QUAL \
+    	 	-f ${ref_genome} \
+   	    	-b rand_ind_sim${sim}.txt \
+     	  	-o OUTFILE_q${QUAL}_s${sim}.${int}.bcf 
+
+      		if [[ -s OUTFILE_q${QUAL}_s${sim}.${int}.bcf ]]
+        		then 
+          			echo "OUTFILE_q${QUAL}_s${sim}.${int}.bcf not empty; moving on"
+        		else
+          			echo "OUTFILE_q${QUAL}_s${sim}.${int}.bcf empty; something went wrong"
+          			exit 1
+   			fi
+
+		cat rand_ind_sim${sim}.txt > names     
+		bcftools reheader -s names OUTFILE_q${QUAL}_s${sim}.${int}.bcf > OUTFILE_q${QUAL}_s${sim}.${int}_RN.bcf
+	fi
+
+
+##############################################
+#### CREATING RAW VARIANTS FILE FROM BAMs ####
+#### DO ONCE FOR EACH MAPPING QUALITY ########
+#### FOR EACH GENOME #########################
+##############################################
+#if false; then
+	if [[ ! -s OUTFILE.q${QUAL}_s${sim}.${int}.vcf ]]; then
+		bcftools call -m \
+                --variants-only \
+                --format-fields GQ \
+                --skip-variants indels \
+                OUTFILE_q${QUAL}_s${sim}.${int}_RN.bcf | bcftools filter \
+                        --set-GTs . \
+                        --include $(printf "QUAL>$QUAL")$(printf "&&")$(printf "FMT/GQ>10") | bcftools view \
+                                --min-alleles 2 \
+                                --max-alleles 2 \
+                                --types snps \
+                                --apply-filter "PASS" \
+                                --output-type v \
+                                --output-file OUTFILE.q${QUAL}_s${sim}.${int}.vcf
+	fi
+
+#fi                
+#                if [[ "$QUAL" -eq 40 ]]; then
+                    echo "Calculating Dxy from calc_dxy_emp script."
+                    ${REF_PATH}/sim_scripts/calc_dxy_emp.sh OUTFILE.q${QUAL}_s${sim}.${int}.vcf $sim $tree_height $int $taxa_ref $day
+                    echo "done calculating Dxy"
+#                else
+#		    		echo "QUAL is $QUAL; not calculating Dxy this time"
+#                fi
+
+## delete bcf file if vcf file is successfully created
+
+	if [[ -f "OUTFILE.q${QUAL}_s${sim}.${int}.vcf" ]]; then
+		echo "VCF file exists and is not empty; deleting BCF file now"
+		rm -f OUTFILE_q${QUAL}_s${sim}.${int}_RN.bcf
+		rm -f OUTFILE_q${QUAL}_s${sim}.${int}.bcf
+	else
+		"VCF file is empty; exiting now"
+		exit 1
+	fi
+                
+#################################
+#### FILTERING WITH VCFTOOLS ####
+### AND INFERRING PHYLOGENIES ###
+#################################
+
+	echo "beginning parallel jobs per maf"
+	export QUAL
+	export sim
+	export REF_PATH
+	export output_dir
+	export day
+	export tree_height
+	export int
+	export miss_list
+	export genes
+
+	parallel --delay 1 --jobs 2  --line-buffer --env genes --env sim --env QUAL --env miss_list --env REF_PATH --env output_dir --env day --env tree_height --env int "bash ${REF_PATH}/sim_scripts/each_maf_emp.sh {}" ::: $maf_list ::: $miss_list
+
+#    mkdir s${sim}_q${QUAL}_miss${miss}_maf${maf}.${int}-${taxa_ref}.phylip_tree_files
+#   	mv *OUTFILE_s${sim}_q${QUAL}_miss${miss}_maf${maf}*.phy s${sim}_q${QUAL}_miss${miss}_maf${maf}.${int}-${taxa_ref}.phylip_tree_files
+#   	mv RAxML* s${sim}_q${QUAL}_miss${miss}_maf${maf}.${int}-${taxa_ref}.phylip_tree_files
+
+				
+####Create a batch file with all normal trees in order by file type, and create a name file
+#cat s${sim}*q*miss*/*bipartitions.*filtered* >> ${output_dir}/${day}-${tree_height}-emp-batch.trees
+#ls s${sim}*q*miss*/*bipartitions.*filtered* >> ${output_dir}/${day}-${tree_height}-emp-tree.names
+
+#rm -f *sim${sim}.${int}*
+
+echo "done with all processes for $tree_height sim${sim} ${int}; exiting now"
+date
