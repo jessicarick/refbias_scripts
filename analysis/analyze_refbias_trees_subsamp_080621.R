fa759507be0b7058936f5c70f73fb94d82ec4421
Jessi Rick <rickx032@umn.edu>
2021-10-06 21:30:51 +0200

    modifications to scripts

--- analysis/analyze_refbias_trees_subsamp_080621.R
+++ analysis/analyze_refbias_trees_subsamp_080621.R
@@ -113,7 +113,10 @@ gt.rf <- read_table2("output/new/092321-LONG-EXT.TTR.gt_rf",col_names=c("sim","g
           simulation = gsub("sim","",sim)) %>%
   add_row(read_table2("output/new/092321-SHORT-EXT.TTR.gt_rf",col_names=c("sim","gt_rf")) %>%
             mutate(height = "SHORT"),
-          simulation = gsub("sim","",sim))
+          simulation = gsub("sim","",sim)) %>%
+  drop_na() %>%
+  group_by(height,sim) %>%
+  slice_head(n=1)
 
 #################
 ## plots of ml tree characteristics
@@ -238,9 +241,9 @@ for (i in 1:length(raxml.trees)){
   }
   
   ##Calculating distances to ML tree
-  results.raxml$RF.Dist.ML[i]<- multiRF(trees.both,multi2di=TRUE)[[2]]
-  #results.raxml$weighted.rf[i] <- wRF.dist(raxml.trees[[i]],ml.tree[[j]]) # this always seems to come out wonky
-  results.raxml$Q.Dist.ML[i] <- Quartet::QuartetDivergence(Quartet::QuartetStatus(trees.both), similarity=FALSE)[-1] #2/3 is max Q
+  results.raxml$RF.Dist.ML[i]<- InfoRobinsonFoulds(trees.both,normalize=TRUE)[1]
+  # #results.raxml$weighted.rf[i] <- wRF.dist(raxml.trees[[i]],ml.tree[[j]]) # this always seems to come out wonky
+  results.raxml$Q.Dist.ML[i] <- Quartet::QuartetDivergence(Quartet::QuartetStatus(trees.both), similarity=FALSE)[-1]/(2/3) #2/3 is max Q
   results.raxml$CI.Dist.ML[i] <- as.matrix(ClusteringInfoDistance(trees.both,normalize = TRUE))[-1,1] #normalized relative to max CI for the tree
 
   ## normalized gamma and imbalance
@@ -255,13 +258,13 @@ for (i in 1:length(raxml.trees)){
    } else {
      ingroup <- trees.both.root[[1]]
    }
-   
+
    if("sim_0_0_0" %in% trees.both.root[[2]]$tip.label){
      ml.ingroup <- drop.tip(trees.both.root[[2]],"sim_0_0_0")
    } else {
      ml.ingroup <- trees.both.root[[2]]
    }
-  
+
   results.raxml$std.ingroup.colless[i] <- colless(as.treeshape(ingroup, model="pda"),norm="pda") - colless(as.treeshape(ml.ingroup,model="pda"),norm="pda")
   results.raxml$std.ingroup.gamma[i] <- gammaStat(chronopl(ingroup,lambda=1))[1] - gammaStat(chronopl(ml.ingroup,lambda=1))[1]
   results.raxml$std.ingroup.sackin[i] <- sackin(as.treeshape(ingroup, model="pda"),norm="pda") - sackin(as.treeshape(ml.ingroup,model="pda"),norm="pda")
@@ -373,10 +376,10 @@ ggarrange(plot1,plot2,plot3,plot4,plot5,plot6,plot7,nrow=1)
 # ggarrange(subsamp.plot1,subsamp.plot2,subsamp.plot3,nrow=1) 
 # 
 
-####################BEGIN ANALYSIS ################################
-####Calculate Matrix of RF distances for all trees with the same species tree in multi tree object
-# results.raxml <- read.csv(file=paste("output/new/",args$output,"-raxml.csv",sep=""),row.names=1,na="NA",header=TRUE)
-# pdf(paste("output/new/",args$output,".pdf",sep=""))
+###################BEGIN ANALYSIS ################################
+###Calculate Matrix of RF distances for all trees with the same species tree in multi tree object
+#results.raxml <- read.csv(file=paste("output/new/",args$output,"-raxml.csv",sep=""),row.names=1,na="NA",header=TRUE)
+pdf(paste("output/new/",args$output,".pdf",sep=""))
 # for (h in unique(as.character(results.raxml$height))){
 #   for (i in unique(as.numeric(as.character(results.raxml$simulation)))){
 #   #for (i in seq(16,30,by=1)){
@@ -385,18 +388,19 @@ ggarrange(plot1,plot2,plot3,plot4,plot5,plot6,plot7,nrow=1)
 #     if (length(subset) != 0) {
 #       trees.subset <- c(raxml.trees[subset],ml.tree[[j]])
 #     } else {
-#       print(paste("no trees for sim",i," for height ",h)) 
+#       print(paste("no trees for sim",i," for height ",h))
 #       next
 #     }
-#     rf_matrix <- multiRF(trees.subset)
-#     
+#     class(trees.subset) <- "multiPhylo"
+#     rf_matrix <- RF.dist(trees.subset)
+# 
 #     ####PcOA of RF distance matrix for plotting trees in tree space
 #     rf_pcoa <- pcoa(rf_matrix)
-#     
+# 
 #     rf_df <- data.frame(axis1=rf_pcoa$vectors[,1],axis2=rf_pcoa$vectors[,2])
 #     biplot <- ggplot(rf_df,aes(axis1,axis2))
 #     plot <- biplot +
-#       geom_jitter(aes(color=as.factor(c(results.raxml$int[subset],"ML_tree")),shape=as.factor(c(results.raxml$method[subset],"ML_tree"))),alpha=0.5,size=5,height=2,width=2)+
+#       geom_jitter(aes(color=as.factor(c(results.raxml$int[subset],"true_tree")),shape=as.factor(c(results.raxml$method[subset],"true_tree"))),alpha=0.5,size=5,height=2,width=2)+
 #       theme_bw()+
 #       theme(legend.text = element_text(size=rel(1.5)),
 #             legend.title = element_blank(),
@@ -404,7 +408,7 @@ ggarrange(plot1,plot2,plot3,plot4,plot5,plot6,plot7,nrow=1)
 #             line = element_line(size=1),
 #             axis.title = element_text(size=rel(1.5)),
 #             axis.text = element_text(size=rel(1)),
-#             panel.grid.major = element_blank(), 
+#             panel.grid.major = element_blank(),
 #             panel.grid.minor = element_blank())+
 #       ggtitle(paste("PCoA on RF Distances, Sim",i,", height ",h, sep=""))+
 #       xlab(paste("PCoA Axis 1 (",round(rf_pcoa$values$Relative_eig[1]*100,1),"%)",sep=""))+
@@ -413,9 +417,9 @@ ggarrange(plot1,plot2,plot3,plot4,plot5,plot6,plot7,nrow=1)
 #       scale_color_manual(labels = c("EXT","INT","truth"),values=c("#009980", "#006699", "black"))+
 #       scale_shape_manual(labels = c("raxml","ml"),values=c(1,16,17))
 #     print(plot)
-#     
+# 
 #     plot2 <- biplot +
-#       geom_jitter(aes(color=as.factor(c(results.raxml$maf[subset],6)),shape=as.factor(c(results.raxml$missing[subset],"true"))),alpha=0.5,size=5,height=2,width=2)+
+#       geom_jitter(aes(color=as.factor(c(results.raxml$maf[subset],"true_tree")),shape=as.factor(c(results.raxml$missing[subset],"true_tree"))),alpha=0.5,size=5,height=2,width=2)+
 #       theme_bw()+
 #       theme(legend.text = element_text(size=rel(1.5)),
 #             legend.title = element_blank(),
@@ -423,7 +427,7 @@ ggarrange(plot1,plot2,plot3,plot4,plot5,plot6,plot7,nrow=1)
 #             line = element_line(size=1),
 #             axis.title = element_text(size=rel(1.5)),
 #             axis.text = element_text(size=rel(1)),
-#             panel.grid.major = element_blank(), 
+#             panel.grid.major = element_blank(),
 #             panel.grid.minor = element_blank())+
 #       ggtitle(paste("PCoA on RF Distances, Sim",i,", height ",h, sep=""))+
 #       xlab(paste("PCoA Axis 1 (",round(rf_pcoa$values$Relative_eig[1]*100,1),"%)",sep=""))+
@@ -432,8 +436,115 @@ ggarrange(plot1,plot2,plot3,plot4,plot5,plot6,plot7,nrow=1)
 #       scale_color_manual(values=c("#009980", "#006699","magenta","lightblue","orange","green","black","pink","turquoise"))+
 #       scale_shape_manual(values=c(0,1,2,3,16,17))
 #     print(plot2)
-#     
+# 
 #     #heatmap(rf_matrix,labCol=FALSE,labRow=paste(results.raxml[subset,]$maf,results.raxml[subset,]$missing,results.raxml[subset,]$int,sep=","),cex.lab=0.5)
 #   }
 # }
 # dev.off()
+
+
+## Plot trees in PCoA space by simulation
+results.raxml <- results.raxml[results.raxml$simulation > 15 & results.raxml$simulation < 26,]
+for (h in unique(as.character(results.raxml$height))){
+  for (i in unique(as.numeric(as.character(results.raxml$simulation)))){
+    #for (i in seq(16,30,by=1)){
+    j <- which(ml.tree.info$simulation == i & ml.tree.info$height == h)
+    subset <- which(results.raxml$simulation == i & results.raxml$height == h & results.raxml$noref == "REF")
+    if (length(subset) != 0) {
+      trees.subset <- c(raxml.trees[subset],ml.tree[[j]])
+    } else {
+      print(paste("no trees for sim",i," for height ",h))
+      next
+    }
+
+  #trees.subset <- sapply(trees.subset,function(x) root(x,outgroup,resolve.root=TRUE))
+  #trees.subset.root <- root(trees.subset,outgroup,resolve.root=TRUE)
+  class(trees.subset) <- "multiPhylo"
+  rf.dist <- RF.dist(trees.subset)
+  rf.pcoa <- pcoa(rf.dist)
+  rf.pcoa.plot <- rf.pcoa$vectors %>%
+    as_tibble() %>%
+    ggplot(aes(x=Axis.1,y=Axis.2)) +
+    geom_point(aes(col=factor(c(results.raxml$maf[subset],"true_tree")),
+                   #text=results.raxml$missing[results.raxml$simulation == s],
+                   shape=factor(c(results.raxml$missing[subset],"true_tree")),
+                   size=factor(c(results.raxml$int[subset],"true_tree"))),
+               alpha=0.6) +
+    theme_bw(base_size=12, base_family="Open Sans") +
+    xlab("PCoA Axis 1") +
+    ylab("PCoA Axis 2") +
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_rect(fill="white", color=NA), 
+      legend.background = element_rect(fill="transparent", colour=NA),
+      legend.key = element_rect(fill="transparent", colour=NA),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.title = element_text(size=18, family="Open Sans Light"),
+      axis.text = element_text(size=14),
+      legend.text = element_text(size=14),
+      legend.title = element_blank()
+    ) +
+    ggtitle(paste0("Subsampled PCoA, Simulation ",i,", ",h)) +
+    geom_hline(yintercept=0,lty=2,col="gray50") +
+    geom_vline(xintercept=0,lty=2,col="gray50") +
+    # biplot arrows for MAF
+    geom_segment(
+      x = 0, y = 0,
+      xend = cor(as.numeric(as.character(results.raxml$maf[subset])), rf.pcoa$vectors[,1][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+      yend = cor(as.numeric(as.character(results.raxml$maf[subset])), rf.pcoa$vectors[,2][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1),
+      lineend = "round", # See available arrow types in example above
+      linejoin = "round",
+      size = 1, 
+      arrow = arrow(length = unit(0.2, "inches")),
+      colour = "black" # Also accepts "red", "blue' etc
+    ) + 
+    annotate(geom = "text",
+             x = cor(as.numeric(as.character(results.raxml$maf[subset])), rf.pcoa$vectors[,1][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+             y = cor(as.numeric(as.character(results.raxml$maf[subset])), rf.pcoa$vectors[,2][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+             label = "MAF",
+             hjust=0,vjust=0
+    ) +
+    # biplot arrows for Missing
+    geom_segment(
+      x = 0, y = 0,
+      xend = cor(as.numeric(as.character(results.raxml$missing[subset])), rf.pcoa$vectors[,1][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+      yend = cor(as.numeric(as.character(results.raxml$missing[subset])), rf.pcoa$vectors[,2][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1),
+      lineend = "round", # See available arrow types in example above
+      linejoin = "round",
+      size = 1, 
+      arrow = arrow(length = unit(0.2, "inches")),
+      colour = "black" # Also accepts "red", "blue' etc
+    ) + 
+    annotate(geom = "text",
+             x = cor(as.numeric(as.character(results.raxml$missing[subset])), rf.pcoa$vectors[,1][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+             y = cor(as.numeric(as.character(results.raxml$missing[subset])), rf.pcoa$vectors[,2][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+             label = "Missing",
+             hjust=1,vjust=1
+    ) +
+    # biplot arrows for INT
+    geom_segment(
+      x = 0, y = 0,
+      xend = cor(as.numeric(as.factor(results.raxml$int[subset])), rf.pcoa$vectors[,1][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+      yend = cor(as.numeric(as.factor(results.raxml$int[subset])), rf.pcoa$vectors[,2][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1),
+      lineend = "round", # See available arrow types in example above
+      linejoin = "round",
+      size = 1, 
+      arrow = arrow(length = unit(0.2, "inches")),
+      colour = "black" # Also accepts "red", "blue' etc
+    ) + 
+    annotate(geom = "text",
+             x = cor(as.numeric(as.factor(results.raxml$int[subset])), rf.pcoa$vectors[,1][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+             y = cor(as.numeric(as.factor(results.raxml$int[subset])), rf.pcoa$vectors[,2][1:length(subset)]) * 0.8 * sqrt(length(subset) - 1), 
+             label = "INT",
+             hjust=0,vjust=0
+    )
+  
+  
+  
+  print(rf.pcoa.plot)
+  #plotly::ggplotly(rf.pcoa.plot)
+  }
+}
+
+dev.off()
